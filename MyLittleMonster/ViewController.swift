5cb266f24e01546b0b79a881b53533eb8e8b4564
shaynemeyer <shayne.meyer@gmail.com>
2015-12-19 08:03:36 -0800

    Added DragImg class that innerits from UIImageView.

--- MyLittleMonster/ViewController.swift
+++ MyLittleMonster/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var heartImg: DragImg!
+    @IBOutlet weak var foodImg: DragImg!
+    
     
 
     override func viewDidLoad() {
